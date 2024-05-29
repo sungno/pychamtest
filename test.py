90a4b404d2671ac411c9192ed649b0221ac9d148
sungno <ysn3939@gmail.com>
2024-05-29 15:37:26 +0900

    second update13

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 # 공통 다운로드 및 로드 기능을 포함한 스크립트에서 필요한 기능을 가져옵니다
 from downloader import download_script, load_module_from_string
 
-# 필요한 모듈 다운로드 및 로드
-method_url = "https://raw.githubusercontent.com/sungno/pychamtest/main/method.py"
-method_content = download_script(method_url)
-load_module_from_string("method", method_content)
+try:
+    # 필요한 모듈 다운로드 및 로드
+    method_url = "https://raw.githubusercontent.com/sungno/pychamtest/main/method.py"
+    method_content = download_script(method_url)
+    load_module_from_string("method", method_content)
 
-# 이제 method 모듈을 사용할 수 있습니다
-from method import *
-print("테스트 수정13!")
-print(a)
-print(b)
-input()
+    # 이제 method 모듈을 사용할 수 있습니다
+    from method import *
+    print("테스트 수정12!")
+    print(a)
+    print(b)
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+input("Press Enter to exit...")
